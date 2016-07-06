a0be404a1fb1db1fdf3319f6d6fc5014518ec83f
James Brunskill <james.brunskill@gmail.com>
2016-07-06 14:47:26 +1200

    Added countdown weekdays remaining option -w (--weekdays)

--- countdown.py
+++ countdown.py
@@ -19,7 +19,7 @@ if not SLACK_URL:
     exit(1)
 
 def days_from_christmas():
-    """Calculates the number of days between the current date and the next 
+    """Calculates the number of days between the current date and the next
     Christmas. Returns the string to displayed.
     """
     currentdate = datetime.now()
@@ -43,57 +43,80 @@ def days_from_date(strdate):
     delta = futuredate - currentdate
     return delta.days + 1
 
-    
-def events(strdate,event):
+def weekdays_from_date(strdate):
+    """ Returns the number of week days between strdate and today. Add one to date
+    as date caclulate is relative to time
+    """
+
+    currentdate = datetime.today()
+    futuredate = datetime.strptime(strdate, '%Y-%m-%d')
+    delta = timedelta(days=1)
+    d = currentdate
+    diff = 0
+    weekend = set([5, 6])
+    while d <= end:
+        if d.weekday() not in weekend:
+            diff += 1
+        d += delta
+
+    return diff
+
+
+def events(strdate,event,weekdays):
     """ Returns string to be displayed with the event mentioned. Sends an error
     if date is incorrect
     """
-    days = days_from_date(strdate)
+    days = weekdays_from_date(strdate) if weekdays else days_from_date(strdate)
+    weekday_str = "week" if weekdays else  ""
+
+
     assert (days >= -2), "Date needs to be in the future"
     if days == -1:
-        return "%d day since %s" % (1,event)
+        return "%d %sday since %s" % (1,weekday_str,event)
     elif days == -2:
-        return "%d days since %s" % (2,event)
+        return "%d %sdays since %s" % (2,weekday_str,event)
     elif days == 1:
-        return "%d day until %s" % (days,event)
+        return "%d %sday until %s" % (days,weekday_str,event)
     else:
-        return "%d days until %s" % (days,event)
+        return "%d %sdays until %s" % (days,weekday_str,event)
 
 
-def date_only(strdate):
+def date_only(strdate,weekdays):
     """ Returns string to be displayed. Sends error message if date is
     in the past
     """
-    days = days_from_date(strdate)
+    days = weekdays_from_date(strdate) if weekdays else days_from_date(strdate)
+    weekday_str = "week" if weekdays else  ""
+
     assert (days >= -2), "Date needs to be in the future"
     futuredate = datetime.strptime(strdate, '%Y-%m-%d')
     if days == -1:
-        return "%d day since %s" % (1, futuredate.strftime("%d %B, %Y"))
+        return "%d %sday since %s" % (1, weekday_str, futuredate.strftime("%d %B, %Y"))
     if days == -2:
-        return "%d days since %s" % (days, futuredate.strftime("%d %B, %Y")) 
+        return "%d %sdays since %s" % (days,weekday_str, futuredate.strftime("%d %B, %Y"))
     if days == 1:
-        return "%d day until %s" % (days, futuredate.strftime("%d %B, %Y")) 
+        return "%d %sday until %s" % (days,weekday_str, futuredate.strftime("%d %B, %Y"))
     else:
-        return "%d days until %s" % (days, futuredate.strftime("%d %B, %Y"))
-    
+        return "%d %sdays until %s" % (days,weekday_str, futuredate.strftime("%d %B, %Y"))
+
 
 
 def post(out):
     """ Posts a request to the slack webhook. Payload can be customized
-    so the message in slack is customized. The variable out is the text 
+    so the message in slack is customized. The variable out is the text
     to be displayed.
-    """    
+    """
 
     payload = {
         "attachments": [
-            {   
+            {
                 "title": "COUNTDOWN!",
                 "text": out,
                 "color": "#7CD197"
             }
         ]
     }
-    
+
     r = requests.post(SLACK_URL, data=json.dumps(payload))
 
 
@@ -101,7 +124,7 @@ def post_error():
     """Sends error message in Slack to alert the user
     about the incorrect date argument
     """
-    
+
     payload = {
         "attachments": [
             {
@@ -113,45 +136,46 @@ def post_error():
             }
         ]
     }
-    
+
     r = requests.post(SLACK_URL, data=json.dumps(payload))
- 
+
 
 @manager.option("-d", "--deadline", dest="date",
-                      help="Specify the deadline in ISO format: yyyy-mm-dd", 
+                      help="Specify the deadline in ISO format: yyyy-mm-dd",
                       metavar="DEADLINE")
-@manager.option("-e", "--event", dest="event", 
+@manager.option("-e", "--event", dest="event",
                       help="Name of the deadline event",metavar="EVENT")
-def deadline(date,event):
+@manager.option("-w", "--weekdays", dest="weekdays",
+                      help="Only include weekdays",metavar="WEEKDAYS")
+
+def deadline(date,event, weekdays):
     """ Method takes two optional arguments. Displays in slack channel
     the number of days till the event. If no arguments are given,
     the number of days till Christmas is displayed.
-    """    
+    """
     try:
         result = ""
         if date:
             if event:
-                result = events(date, event)
+                result = events(date, event, weekdays)
             else:
-                result = date_only(date)
+                result = date_only(date,weekdays)
         else:
             result = days_from_christmas()
     except:
         post_error()
     else:
         post(result)
-        
+
 
 
 @manager.command
 def initiate():
     payload = { "text": "App is now connected to your Slack Channel."}
     r = requests.post(SLACK_URL, data=json.dumps(payload))
-    
-    
 
-    
+
+
+
 if __name__ == "__main__":
     manager.run()
-
-
